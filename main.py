936f37da329212c010754f3d39d1757624f2d7ea
Arihant1302 <dholearihant123@gmail.com>
2023-01-08 17:22:57 +0530

    08/01/2023

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from best_model_finder import best_model_
 from flask import Flask, render_template, request
-app = Flask(__name__,template_folder='templates')
+app = Flask(__name__,static_folder="static",template_folder='templates')
 
 @app.route('/') # To render Homepage
 def home_page():
@@ -15,7 +15,10 @@ def submit():
         etest_s = int(request.form.get("etest_p"))
         work_exp = int(request.form.get("work-ex"))
         prediction = best_model_.placement_predictor(ssc_s,hsc_s,deg_s,etest_s,work_exp)
-    return render_template('submit.html',test=prediction)
+        if(prediction=="Placed"):
+            return render_template("test.html")
+        else:
+            return render_template('test2.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
